9fec48b0f93f9c658e96ba124dd8ee1fd11dfb5b
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-06-06 12:11:28 -0400

    new changes

--- PracticeFiles/displayingList.jsx
+++ PracticeFiles/displayingList.jsx
@@ -1,38 +1,45 @@
-
 const myFriends = [
     {
         First: 'Johan', 
         Last: 'Gallardo', 
         Age: 22, 
         OtherNames: 'Joan',
+        Site: 'https://www.youtube.com',
     },
     {
         First: 'Michael',
         Last: 'Kassie',
         Age: 22, 
         OtherNames: 'N/A',
+        Site: 'https://www.linkedin.com',
     },
     {
         First: 'Juvenson',
         Last: 'Pierre', 
         Age: 22, 
         OtherNames: 'N/A', 
+        Site: 'https://www.google.com'
     },
-]
-
-function App() {
+  ]
+  
+  function App() {
     return(
+      <div>
+        <h1>Amigos</h1>
         <ul>
-            {myFriends.map(
-                function(item){
-                    <li>{item.First} {item.Last}</li>
-                }
-            )}
+            {myFriends.map(function(item){
+              return(
+                <li>
+                  <span> {item.First}</span>
+                  <span> {item.Last} </span>
+                  <span> 
+                    <a href={item.Site}>site here</a>
+                  </span>
+                </li>
+              ); 
+            })}
         </ul>
-    )
-}
-
-
-
-
-
+      </div>
+    );
+  }
+  
