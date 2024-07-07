12e77494fdf3581ca6d587fe4234cf12734872ae
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-07-07 19:54:09 -0400

    Update 11SideEffect2.jsx

---
+++ 6PROPS/11SideEffect2.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+function App() {
+    return(
+        <div>
+            <label htmlFor= "search">Search: </label>
+            <input id = "search" type = "text"/> 
+        </div>
+    );
+}
+
+const SearchBar = () => {
+    
+}
+
+export default App; 
