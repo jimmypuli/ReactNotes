eaa3eba18fac5583226d97ddc17aed1a7baa57e7
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-07-08 11:55:27 -0400

    Update 11SideEffect2.jsx

--- 6PROPS/11SideEffect2.jsx
+++ 6PROPS/11SideEffect2.jsx
@@ -1,16 +1,26 @@
 import * as React from 'react'
 
 function App() {
-    return(
-        <div>
-            <label htmlFor= "search">Search: </label>
-            <input id = "search" type = "text"/> 
-        </div>
-    );
+  const appHandleSearch = (event) => {
+    console.log(event.target.value)
+  }
+  return(
+    <div>
+      <SearchBar searchEvent = {appHandleSearch}/>
+    </div>
+  );
 }
 
-const SearchBar = () => {
-    
+const SearchBar = (props) => {
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  }
+  return(
+    <div>
+      <label htmlFor= "search">Search: </label>
+      <input id = "search" type = "text" onChange={props.searchEvent}/> 
+    </div>
+  );
 }
 
 export default App; 
