99ed5d574d8a1c66c448e90292f4935c6b8eb108
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-07-09 11:46:34 -0400

    Create 14SearchHistory.jsx

---
+++ 6PROPS/14SearchHistory.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+function App() {
+  const names = ["Fido", "Chain", "Link", "Fogell", "Evan", "Seth", "Jackie"];
+  const [searchTerm, setSearchTerm] = React.useState(''); 
+
+  const appHandleSearch = (event) => {
+    setSearchTerm(event.target.value); 
+  }
+  const searchedNames = names.filter((name) => {
+    return name.toLowerCase().includes(searchTerm.toLowerCase()); 
+  }); 
+
+
+  return(
+    <div>
+      <SearchBar searchEvent = {appHandleSearch}/>
+      <DisplayList namesList = {searchedNames}/> 
+    </div>
+  );
+}
+
+const SearchBar = (props) => {
+  return(
+    <div>
+      <label htmlFor= "search">Search: </label>
+      <input id = "search" type = "text" onChange={props.searchEvent}/> 
+    </div>
+  );
+}
+
+const DisplayList = ({namesList}) => {
+  return(
+    <ul>
+      {namesList.map((name) => (
+        <li>{name}</li>
+      ))}
+    </ul>
+  ); 
+}
+
+export default App; 
