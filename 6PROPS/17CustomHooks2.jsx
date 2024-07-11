4aa7ebee6c6efe45205e2959c5487ac629b7a731
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-07-11 16:13:29 -0400

    Create 17CustomHooks2.jsx

---
+++ 6PROPS/17CustomHooks2.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+
+function App() {
+  const names = ["Fido", "Chain", "Link", "Fogell", "Evan", "Seth", "Jackie"];
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('searchHistory') || ''
+  ); 
+
+  React.useEffect(() => {
+    localStorage.setItem('searchHistory', searchTerm);
+  }, [searchTerm]);
+
+  const appHandleSearch = (event) => {
+    setSearchTerm(event.target.value); 
+  }
+  const searchedNames = names.filter((name) => {
+    return name.toLowerCase().includes(searchTerm.toLowerCase()); 
+  }); 
+
+
+  return(
+    <div>
+      <SearchBar searchHist = {searchTerm} searchEvent = {appHandleSearch}/>
+      <DisplayList namesList = {searchedNames}/> 
+    </div>
+  );
+}
+
+const SearchBar = (props) => {
+  return(
+    <div>
+      <label htmlFor= "search">Search: </label>
+      <input id = "search" type = "text" value = {props.searchHist} onChange={props.searchEvent}/> 
+    </div>
+  );
+}
+
+const DisplayList = ({namesList}) => {
+  return(
+    <ul>
+      {namesList.map((name, index) => (
+        <li key = {index}>{name}</li>
+      ))}
+    </ul>
+  ); 
+}
+
+export default App; 
