d9d9f9f1ac198cf05c62c8a95ad57011da5e0cb3
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2024-07-16 12:43:40 -0400

    Create 22rickMortyInlineH.jsx

---
+++ 6PROPS/22rickMortyInlineH.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+const RickMortyCharacters = [
+    {
+        Name: "Rick Sanchez",
+        Age: "58",
+        Gender: "M"
+    },
+    {
+        Name: "Morty Smith",
+        Age: "15", 
+        Gender: "M" 
+    },
+    {
+        Name: "Diane Sanchez", 
+        Age: "40", 
+        Gender: "F"
+    }, 
+    {
+        Name: "Jerry Smith",
+        Age: "40", 
+        Gender: "M"
+    },
+    {
+        Name: "Summer Smith", 
+        Age: "17", 
+        Gender: "F"
+    }
+]
+
+export default App; 
